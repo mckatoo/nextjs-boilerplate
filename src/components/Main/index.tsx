3e336d1ad832cd48422685835714de7af7f39b53
Milton Carlos Katoo <mckatoo@gmail.com>
2020-07-09 16:52:45 -0300

    feat: add storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as Styles from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'DevNaRoça',
+  description = 'Desenvolvimento Web, Mobile e Desktop'
+}) => (
   <Styles.Wrapper>
     <Styles.Logo
       src="/img/logo.svg"
       alt="Imagem DevNaRoça - Chapéu óculos amarelo e azul com gravata borboleta"
     />
-    <Styles.Title>React Avançado</Styles.Title>
-    <Styles.Description>
-      Typescript, ReactJS, NextJS e Styled Components
-    </Styles.Description>
+    <Styles.Title>{title}</Styles.Title>
+    <Styles.Description>{description}</Styles.Description>
     <Styles.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para telas de código."
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,31 +7,74 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /DevNaRoça/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        background-color: #06092b;
+        color: #fff;
+        width: 100%;
+        height: 100%;
+        padding: 3rem;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-flex-direction: column;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        text-align: center;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        -webkit-justify-content: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+      }
+
+      .c1 {
+        width: 25rem;
+        margin-bottom: 2rem;
+      }
+
+      .c2 {
+        font-size: 2.5rem;
+      }
+
+      .c3 {
+        font-size: 2rem;
+        font-weight: 400;
+      }
+
+      .c4 {
+        margin-top: 3rem;
+        width: min(30rem,100%);
+      }
+
       <main
-        class="styles__Wrapper-sc-1cs6c8-0 jODcew"
+        class="c0"
       >
         <img
           alt="Imagem DevNaRoça - Chapéu óculos amarelo e azul com gravata borboleta"
-          class="styles__Logo-sc-1cs6c8-1 kjzhTH"
+          class="c1"
           src="/img/logo.svg"
         />
         <h1
-          class="styles__Title-sc-1cs6c8-2 gcXVYJ"
+          class="c2"
         >
-          React Avançado
+          DevNaRoça
         </h1>
         <h2
-          class="styles__Description-sc-1cs6c8-3 epwofi"
+          class="c3"
         >
-          Typescript, ReactJS, NextJS e Styled Components
+          Desenvolvimento Web, Mobile e Desktop
         </h2>
         <img
           alt="Um desenvolvedor de frente para telas de código."
-          class="styles__Illustration-sc-1cs6c8-4 kYgUhr"
+          class="c4"
           src="/img/hero-illustration.svg"
         />
       </main>
