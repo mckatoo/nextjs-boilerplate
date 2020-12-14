274f3e34a4d7698bae6d43b73ed900c16f7e1c66
Milton Carlos Katoo <mckatoo@gmail.com>
2020-12-14 14:09:57 -0300

    feat: split generators for components and templates

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,11 +1,38 @@
 module.exports = function (plop) {
-  plop.setGenerator('controller', {
-    description: 'application controller logic',
+  plop.setGenerator('template', {
+    description: 'application template logic',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'controller name please'
+        message: 'template name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      },
+    ]
+  })
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
       }
     ],
     actions: [
