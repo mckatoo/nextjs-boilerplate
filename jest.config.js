1c81b31816bb49a7b4fb6a40e800271db95fea72
Milton Carlos Katoo <mckatoo@gmail.com>
2020-12-14 15:35:01 -0300

    style: added styled-components theme

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  testEnvironmanet: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/'
+  ],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/components/**/*mock*.ts',
+    '!src/types/**/*'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  },
+  watchPathIgnorePatterns: [
+    'stories.tsx'
+  ]
 }
